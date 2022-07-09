67bc7bf2ae7c87a917dc30ca187615b1ba84cd3f
Eloilson <eloilson.ageedu@gmail.com>
2022-07-09 18:57:58 -0300

    feat: created migrations of categories

---
+++ database/migrations/1657402125116_categories.ts
@@ -0,0 +1,28 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'categories'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('name').notNullable()
+      table.string('description')
+      table.string('position').notNullable()
+      table.boolean('active').notNullable().defaultTo(true)
+      table
+        .integer('establishment_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('establishments')
+        .onDelete('RESTRICT')
+      table.timestamp('deleted_at')
+      table.timestamps(true, true)
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
