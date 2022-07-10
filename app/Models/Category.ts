b866b0a3306a8d2d6e3e8a02a9e59cd06f997ad2
Eloilson <eloilson.ageedu@gmail.com>
2022-07-10 11:45:22 -0300

    feat: add category  model

--- app/Models/Category.ts
+++ app/Models/Category.ts
@@ -1,13 +1,35 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import Product from './Product'
 
 export default class Category extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public description: string
+
+  @column()
+  public position: string
+
+  @column()
+  public active: boolean
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public deletedAt: DateTime | null
+
+  @hasMany(() => Product, {
+    foreignKey: 'category_id',
+    localKey: 'id',
+  })
+  public products: HasMany<typeof Product>
 }
