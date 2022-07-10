b4171eb2bb6bccddc5fcb8fbfcaf91d84d940bff
Eloilson <eloilson.ageedu@gmail.com>
2022-07-10 11:45:33 -0300

    feat: add product  model

--- app/Models/Product.ts
+++ app/Models/Product.ts
@@ -5,9 +5,36 @@ export default class Product extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  public category_id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public description: string | null
+
+  @column()
+  public image: string | null
+
+  @column()
+  public price: number
+
+  @column()
+  public unity: string
+
+  @column()
+  public position: string
+
+  @column()
+  public active: boolean
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public deletedAt: DateTime | null
 }
