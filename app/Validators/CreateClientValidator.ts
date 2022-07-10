91a363e4cbfe5232e7edefb2db2cde07ca73c121
Eloilson <eloilson.ageedu@gmail.com>
2022-07-10 10:17:21 -0300

    refactor: add custom error messages

---
+++ app/Validators/CreateClientValidator.ts
@@ -0,0 +1,59 @@
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class CreateClientValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string({}, [ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string({}, [
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create({
+    name: schema.string({ trim: true }, [rules.minLength(3), rules.maxLength(255)]),
+    email: schema.string({ trim: true }, [
+      rules.email(),
+      rules.unique({ table: 'users', column: 'email' }),
+    ]),
+    password: schema.string({}, [rules.minLength(8), rules.maxLength(36), rules.confirmed()]),
+    telefone: schema.string({ trim: true }, [
+      rules.mobile({ locale: ['pt-BR'] }),
+      rules.maxLength(15),
+    ]),
+  })
+
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages: CustomMessages = {
+    'required': '{{ field }} é obrigatório para o cadastro',
+    'email.email': '{{ field }} deve ser no formato válido',
+    'email.unique': '{{ field }} já está em uso',
+    'password.minLength': '{{ field }} deve ter no mínimo 8 caracteres',
+    'password.maxLength': '{{ field }} deve ter no máximo 36 caracteres',
+    'password.confirmed': '{{ field }} as senhas não conferem',
+    'telefone.mobile': '{{ field }} deve ser no formato válido',
+  }
+}
