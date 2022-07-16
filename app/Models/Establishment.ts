df41ba8f246824ae05df50396c000af910b9e8fd
Eloilson <eloilson.ageedu@gmail.com>
2022-07-16 14:38:26 -0300

    feat: create establishment model

--- app/Models/Establishment.ts
+++ app/Models/Establishment.ts
@@ -1,5 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany, manyToMany, ManyToMany } from '@ioc:Adonis/Lucid/Orm'
+import Category from './Category'
+import PaymentMethod from './PaymentMethod'
 
 export default class Establishment extends BaseModel {
   @column({ isPrimary: true })
@@ -25,4 +27,19 @@ export default class Establishment extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasMany(() => Category, {
+    foreignKey: 'establishment_id',
+    localKey: 'id',
+  })
+  public categories: HasMany<typeof Category>
+
+  @manyToMany(() => PaymentMethod, {
+    pivotTable: 'establishments_payments_methods',
+    localKey: 'id',
+    pivotForeignKey: 'establishment_id',
+    relatedKey: 'id',
+    pivotRelatedForeignKey: 'payment_method_id',
+  })
+  public payments_methods: ManyToMany<typeof PaymentMethod>
 }
