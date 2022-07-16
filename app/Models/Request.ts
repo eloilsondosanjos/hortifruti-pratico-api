28548b40dba82edcb8838ea3a88011e34f9cf67c
Eloilson <eloilson.ageedu@gmail.com>
2022-07-16 14:38:50 -0300

    feat: create request model

--- app/Models/Request.ts
+++ app/Models/Request.ts
@@ -1,5 +1,11 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, HasMany, hasMany, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import Client from './Client'
+import RequestStatus from './RequestStatus'
+import Establishment from './Establishment'
+import RequestProduct from './RequestProduct'
+import RequestAddress from './RequestAddress'
+import PaymentMethod from './PaymentMethod'
 
 export default class Request extends BaseModel {
   @column({ isPrimary: true, serializeAs: null })
@@ -37,4 +43,40 @@ export default class Request extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @hasOne(() => Client, {
+    foreignKey: 'id',
+    localKey: 'client_id',
+  })
+  public client: HasOne<typeof Client>
+
+  @hasMany(() => RequestStatus, {
+    foreignKey: 'request_id',
+    localKey: 'id',
+  })
+  public request_status: HasMany<typeof RequestStatus>
+
+  @hasOne(() => Establishment, {
+    foreignKey: 'id',
+    localKey: 'establishment_id',
+  })
+  public establishment: HasOne<typeof Establishment>
+
+  @hasMany(() => RequestProduct, {
+    foreignKey: 'request_id',
+    localKey: 'id',
+  })
+  public products: HasMany<typeof RequestProduct>
+
+  @hasOne(() => RequestAddress, {
+    foreignKey: 'id',
+    localKey: 'request_address_id',
+  })
+  public address: HasOne<typeof RequestAddress>
+
+  @hasOne(() => PaymentMethod, {
+    foreignKey: 'id',
+    localKey: 'payment_method_id',
+  })
+  public payment_method: HasOne<typeof PaymentMethod>
 }
